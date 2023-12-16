a1de5548bc4cb6a8c8f415103ec805e865345803
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-16 20:51:22 +0530

    Create Admin Class

---
+++ Student_Registration_System-Backend/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Registration_System_Backend.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
