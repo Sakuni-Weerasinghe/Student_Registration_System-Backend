bd01edf1790008ac37d49484a5a410a98fe53121
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-16 21:00:34 +0530

    Configure service for create database

--- Student_Registration_System-Backend/Program.cs
+++ Student_Registration_System-Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Student_Registration_System_Backend.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("StudentRegistrationConnectionString"));
+});
 
 var app = builder.Build();
 
