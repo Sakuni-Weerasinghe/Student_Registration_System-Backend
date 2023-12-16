9f5fb76f7f80301227b38e1c80ab535acaf3ac9b
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-16 20:56:13 +0530

    Create DbContext class

---
+++ Student_Registration_System-Backend/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Registration_System_Backend.Models;
+
+namespace Student_Registration_System_Backend.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>().ToTable("admins");
+
+        }
+    }
+}
