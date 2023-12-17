c692eee078bc3e8a33256c55eb6fe904e834742f
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-17 22:13:35 +0530

    Create course class

---
+++ Student_Registration_System-Backend/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Registration_System_Backend.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+
+        public int Credits { get; set; }
+        public string Description { get; set; }
+        public string Lecturer { get; set; }
+
+  
+
+    }
+}
