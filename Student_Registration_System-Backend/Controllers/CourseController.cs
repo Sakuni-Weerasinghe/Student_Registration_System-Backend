761d63cc8ca39f7a78bb101854d6dfdfe78d5594
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-17 22:27:16 +0530

    Implement HttpPost method to addcourse

---
+++ Student_Registration_System-Backend/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Student_Registration_System_Backend.Context;
+using Student_Registration_System_Backend.Models;
+
+namespace Student_Registration_System_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public CourseController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+        [HttpPost("add-course")]
+        public async Task<IActionResult> AddCourse([FromBody] Course addCourseRequest)
+        {
+            await _authContext.Courses.AddAsync(addCourseRequest);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(addCourseRequest);
+        }
+    }
+}
