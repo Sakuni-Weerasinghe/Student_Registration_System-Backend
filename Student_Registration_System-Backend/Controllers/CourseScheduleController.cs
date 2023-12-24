620df04cb66252e096ddda35ae4c7c6092850113
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-24 21:17:41 +0530

    Implelement method to add course schedules for databse

--- Student_Registration_System-Backend/Controllers/CourseScheduleController.cs
+++ Student_Registration_System-Backend/Controllers/CourseScheduleController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Student_Registration_System_Backend.Context;
 using Student_Registration_System_Backend.Models;
+using Student_Registration_System_Backend.NewFolder;
 
 namespace Student_Registration_System_Backend.Controllers
 {
@@ -15,13 +18,51 @@ namespace Student_Registration_System_Backend.Controllers
             _authContext = appDbContext;
         }
 
-       // [HttpPost("add-course-schedule")]
-      //  public async Task<IActionResult> AddCourseSchedule([FromBody] CourseSchedule addCourseScheduleRequest)
-       // {
-        //    await _authContext. .AddAsync(addCourseScheduleRequest);
-        //    await _authContext.SaveChangesAsync();
 
-        //    return Ok(addCourseScheduleRequest);
-      //  }
+
+        [HttpPost("add-course-schedule")]
+        public async Task<IActionResult> AddCourseSchedule([FromBody] CourseScheduleRequestDto addCourseScheduleRequest)
+        {
+            // Step 1: Retrieve CourseId for the given CourseCode
+            var course = _authContext.Courses.FirstOrDefault(c => c.CourseCode == addCourseScheduleRequest.CourseCode);
+
+            if (course == null)
+            {
+                // Handle the case where the CourseCode is not found
+                return NotFound($"Course with code {addCourseScheduleRequest.CourseCode} not found");
+            }
+
+            // Step 2: Check if the schedule already exists for any course
+            var existingSchedule = _authContext.CourseSchedules
+                .FirstOrDefault(s => s.Date == addCourseScheduleRequest.Date &&
+                                      s.Time == addCourseScheduleRequest.Time &&
+                                      s.Venue == addCourseScheduleRequest.Venue);
+
+            if (existingSchedule != null)
+            {
+                // Handle the case where the schedule already exists
+                return Conflict(new
+                {
+                    Message = "Schedule already exists"
+                });
+            }
+
+            // Step 4: Create a new instance of CourseSchedule
+            var newSchedule = new CourseSchedule
+            {
+                CourseId = course.CourseId,
+                Date = addCourseScheduleRequest.Date,
+                Time = addCourseScheduleRequest.Time,
+                Venue = addCourseScheduleRequest.Venue
+            };
+
+            // Step 5: Save the new CourseSchedule to the database
+            _authContext.CourseSchedules.Add(newSchedule);
+            _authContext.SaveChanges();
+
+            // Optionally, you can return a response indicating success
+            return Ok(new { Message = "Course schedule added successfully" });
+
+        }
     }
 }
---
+++ Student_Registration_System-Backend/Dto/CourseScheduleRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Registration_System_Backend.NewFolder
+{
+    public class CourseScheduleRequestDto
+    {
+        public string CourseCode { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Venue { get; set; }
+    }
+}
